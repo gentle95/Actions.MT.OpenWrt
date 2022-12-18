144a602c74ab5e702017381e0dff0a71b1b20900
gentle95 <loong.zou85@gmail.com>
2022-12-18 14:25:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,7 +12,7 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-#echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 echo "src-git openclash https://github.com/vernesong/OpenClash.git" >> "feeds.conf.default"
 
 # Add a feed source
