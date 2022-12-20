20936691b7a332b1f20e535e7d746d73ee3f41ab
gentle95 <loong.zou85@gmail.com>
2022-12-20 18:46:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 echo "src-git openclash https://github.com/vernesong/OpenClash.git" >> "feeds.conf.default"
+echo "src-git luci-theme-neobird https://github.com/thinktip/luci-theme-neobird.git" >> "feeds.conf.default"
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
