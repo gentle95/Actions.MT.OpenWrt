49c1373cb9ffe05685f4de0c98413dd03d728a6b
gentle95 <loong.zou85@gmail.com>
2023-03-01 11:02:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,4 +20,4 @@ echo "src-git openclash https://github.com/vernesong/OpenClash.git" >> "feeds.co
 #git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-app-serverchan
 #rm -rf package/lean/luci-app-pushbot
 #git clone https://github.com/zzsj0928/luci-app-pushbot.git package/lean/luci-app-pushbot
-git clone https://github.com/thinktip/luci-theme-neobird.git package/lean/luci-theme-neobird
+#git clone https://github.com/thinktip/luci-theme-neobird.git package/lean/luci-theme-neobird
