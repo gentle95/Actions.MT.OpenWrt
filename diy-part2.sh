ebb34841d105ddf971f7c6d524d18509fbdedc17
gentle95 <loong.zou85@gmail.com>
2023-05-01 21:42:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,4 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-sed -i 's/root::0:0:99999:7:::/root:$1$xAG3abfV$3BPV8nNAm8lzyEtiPjOHZ0:19478:0:99999:7:::/g' package/base-files/files/etc/shadow
+sed -i 's/root:::0:99999:7:::/root:$1$xAG3abfV$3BPV8nNAm8lzyEtiPjOHZ0:19478:0:99999:7:::/g' package/base-files/files/etc/shadow
